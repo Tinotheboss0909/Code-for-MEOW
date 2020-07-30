93212fa49a2e6756c02411c05113b7a9dc9dc9e2
Glitch (prairie-fierce-cornflower) <none>
2020-07-30 22:15:36 +0000

    Yay ya ya

---
+++ index.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+const Discord = require('discord.js');
+const { prefix, token } = require('./config.json');
+
+const client = new Discord.Client();
+client.commands = new Discord.Collection();
+
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+
+	// set a new item in the Collection
+	// with the key as the command name and the value as the exported module
+	client.commands.set(command.name, command);
+}
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.on('message', message => {
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+	const args = message.content.slice(prefix.length).trim().split(/ +/);
+	const command = args.shift().toLowerCase();
+
+	if (command === 'ping') {
+client.commands.get('ping').execute(message, args);	
+ } else if (command === 'beep') {
+		message.channel.send('Boop.');
+	}
+	// other commands...
+});
+
+client.login(token);
